d70ca6f512494d4b3cdd670db2d9817ae99484b5
brittmoni <brittfranklin92@gmail.com>
2021-08-03 22:23:14 -0400

    Committing file structure

---
+++ src/InputCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+
+  public static void inputThenPrintSumAndAverage() {
+    Scanner scanner = new Scanner(System.in);
+
+
+
+
+    scanner.close();
+  }
+}
